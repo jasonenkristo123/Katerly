0db0a7a73c95d9434cc8689c765f5c42fa5da61a
jasonenkristo123 <jasonenkristo@gmail.com>
2026-04-25 20:04:11 +0700

    test: testing branch dev

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,10 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <h1>Hello world</h1>
+    <section>
+      <p>
+        This is branch dev
+      </p>
+    </section>
   );
 }
