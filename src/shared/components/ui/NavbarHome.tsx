27c31ca4bb237a8a28973f0ffbbe92d2c4ac0b3a
jasonenkristo123 <jasonenkristo@gmail.com>
2026-05-05 22:35:20 +0700

    chore: make feature file

--- src/shared/components/ui/NavbarHome.tsx
+++ src/shared/components/ui/NavbarHome.tsx
@@ -2,13 +2,13 @@
 
 export default function NavbarHome() {
     return (
-        <div className="w-full bg-white flex justify-between items-center px-12 py-4 fixed z-999 ">
+        <div className="w-full bg-white flex justify-between items-center px-12 py-2 fixed z-999 ">
             <div>
                 <h1 className="font-anonymous-700 text-[56px]">
                     Kater<span className="text-green-bold">Ly</span>
                 </h1>
             </div>
-            <div className="flex gap-10">
+            <div className="flex gap-10 font-poppins-400 text-[21px]">
                 <p>
                     Fitur
                 </p>
