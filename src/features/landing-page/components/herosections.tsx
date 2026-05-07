10d585022ce4556aae7e3c3f2ed65e71f1dab5b5
jasonenkristo123 <jasonenkristo@gmail.com>
2026-05-07 19:14:12 +0700

    fix: fixing css style in hero sections

--- src/features/landing-page/components/herosections.tsx
+++ src/features/landing-page/components/herosections.tsx
@@ -27,7 +27,7 @@ export default function HeroSections() {
     return (
         <section className="bg-hero-gradient w-full min-h-screen pt-36 pb-20 px-6 md:pt-40 md:px-12 lg:pt-48 lg:px-16 xl:pt-[180px] xl:px-[100px] flex flex-col lg:flex-row items-center gap-12 lg:gap-8 xl:gap-0 overflow-hidden">
             <div className="w-full lg:w-1/2 flex flex-col space-y-6 lg:space-y-8">
-                <div className="flex items-center w-max max-w-full gap-3 rounded-[12px] border border-green-bold/30 bg-green-superlight px-3 py-1.5">
+                <div className="flex items-center w-max max-w-full gap-3 rounded-full border border-green-bold/30 bg-green-superlight px-3 py-1.5">
                     <div className="w-[10px] h-[10px] rounded-full bg-green-primary shrink-0" />
                     <p className="text-[12px] sm:text-[14px] lg:text-[14px] xl:text-[16px] font-poppins-500 text-green-primary truncate">
                         Khusus untuk UMKM Catering Indonesia
@@ -47,7 +47,7 @@ export default function HeroSections() {
 
                 <div className="flex flex-col sm:flex-row items-center gap-4 sm:gap-7 w-full sm:w-auto">
                     <Link href="/register" className="w-full sm:w-auto">
-                        <Button variant="primary" size="lg" className="w-full sm:w-auto shadow-black/50 shadow-lg justify-center" >
+                        <Button variant="primary" size="lg" className="w-full sm:w-auto shadow-black/50 shadow-lg justify-center lg:px-2 xl:px-12" >
                             Coba gratis sekarang
                             <ArrowRight className="ml-2" />
                         </Button>
@@ -69,12 +69,12 @@ export default function HeroSections() {
                 </div>
 
             </div>
-            
+
             <div className="hidden lg:flex w-full lg:w-1/2 items-center justify-center mt-12 lg:mt-0 relative">
                 <div className="relative w-full max-w-[400px] xl:max-w-[563px]">
                     <Image src="/images/hero-bg.webp" width={563} height={532} alt="hero bg" className="w-full h-auto object-contain relative z-10" />
 
-                    <div className="flex flex-col bg-white items-start justify-center px-4 sm:px-6 py-2 rounded-2xl w-[140px] xl:w-[162px] h-auto shadow-gray/30 shadow-xl absolute top-[5%] xl:top-[15%] -left-[5%] xl:-left-[5%] z-20">
+                    <div className="flex flex-col bg-white items-start justify-center px-4 sm:px-6 py-2 rounded-2xl w-[140px] xl:w-[162px] h-auto shadow-gray/30 shadow-xl absolute top-[5%] xl:top-[6%] -left-[5%] xl:-left-[5%] z-20">
                         <p className="font-poppins-400 text-graytext-secondary text-[12px] xl:text-[16px]">
                             HPP/Porsi
                         </p>
@@ -83,7 +83,7 @@ export default function HeroSections() {
                         </p>
                     </div>
 
-                    <div className="bg-white rounded-2xl shadow-gray/30 shadow-xl absolute flex items-center justify-start px-3 py-3 w-[180px] xl:w-[234px] bottom-[10%] xl:bottom-[20%] -right-[5%] xl:-right-[5%] z-20">
+                    <div className="bg-white rounded-2xl shadow-gray/30 shadow-xl absolute flex items-center justify-start px-3 py-3 w-[180px] xl:w-[234px] bottom-[10%] xl:bottom-[5%] -right-[5%] xl:-right-[20%] z-20">
                         <div className="flex gap-2 xl:gap-3 items-center">
                             <SvgWrapper src="/images/upgraph.svg" className="w-[32px] h-[32px] xl:w-[46px] xl:h-[46px] shrink-0" color="text-green-primary fill-current" />
                             <div>
