67db315233c377066edd7dfe05c3009c2806dc4c
jasonenkristo123 <jasonenkristo@gmail.com>
2026-05-16 11:32:43 +0700

    fix: fix error google login

--- src/features/auth/components/LoginPage.tsx
+++ src/features/auth/components/LoginPage.tsx
@@ -16,13 +16,7 @@ export default function LoginPage() {
     const router = useRouter();
     const { mutateAsync } = useLogin();
     const { mutateAsync: mutateAsyncGoogle } = useLoginGoogle();
-    const [uxMode, setUxMode] = useState<"popup" | "redirect">("popup");
 
-    useEffect(() => {
-        if (typeof window !== "undefined") {
-            setUxMode(window.innerWidth > 768 ? "popup" : "redirect");
-        }
-    }, []);
     const {
         register,
         handleSubmit,
@@ -180,7 +174,7 @@ export default function LoginPage() {
                                     text="signin_with"
                                     theme="outline"
                                     shape="rectangular"
-                                    ux_mode={uxMode}
+                                    ux_mode={window.innerWidth > 768 ? "popup" : "redirect"}
                                     onSuccess={handleGoogleLogin}
                                     onError={() => {
                                         console.log("Login Failed")
